97fca5a011b0c3639df14ee10c9535c369730799
Juan Cadenas <jcadenas1899@gmail.com>
2024-06-29 01:01:22 -0300

    app casi completa

--- data/app.R
+++ data/app.R
@@ -36,17 +36,54 @@ ui <- fluidPage(
   )
            ),
   tabPanel("Nivel de Educacion",
+   sidebarLayout(
+    sidebarPanel(
+       selectInput('Educacion', 'Seleccionar nivel de educacion', unique(receptivo_fecha$Estudio))
+     ),
+    mainPanel(
+      h2("Gastos por Persona según nivel de educacion", align = "center"),
+       plotOutput("gastosPlote")
+     )
+   )
            ),
   tabPanel("Ocupacion",
+   sidebarLayout(
+    sidebarPanel(
+       selectInput('ocupacion', 'Seleccionar ocupacion', unique(receptivo_fecha$Ocupacion))
+   ),
+   mainPanel(
+     h2("Gastos por Persona según ocupacion", align = "center"),
+      plotOutput("gastosPloto")
+     )
+   )
            ),
   tabPanel("Transporte de Ingreso",
+   sidebarLayout(
+    sidebarPanel(
+       selectInput('ingreso', 'Seleccionar transporte de ingreso', unique(receptivo_fecha$`Transporte Internacional de Ingreso`))
+   ),
+  mainPanel(
+    h2("Gastos por Persona según metodo de ingreso", align = "center"),
+      plotOutput("gastosPloti")
+     )
+   )
            ),
-  tabPanel("Transporte de Egreso")
+  tabPanel("Transporte de Egreso",
+   sidebarLayout(
+    sidebarPanel(
+      selectInput('egreso', 'Seleccionar metodo de egreso', unique(receptivo_fecha$`Transporte Internacional de Egreso`))
+   ),
+   mainPanel(
+     h2("Gastos por Persona según metodo de ingreso", align = "center"),
+        plotOutput("gastosPloteg")
+             )
+    ))
   )
 )
+
 server <- function(input, output) {
   output$gastosPlot <- renderPlot({
-    datos_filtrados <- receptivo_fecha %>% 
+    datos_filtrados <- receptivo_fecha %>%    ##motivo
       filter(Motivo == input$motivo) %>%
       select(Motivo, all_of(columnas_gasto))
     
@@ -74,7 +111,7 @@ server <- function(input, output) {
   
   output$gastosPlotn <- renderPlot({
     datos_filtrados <- receptivo_fecha %>% 
-      filter(Pais == input$Pais) %>%
+      filter(Pais == input$nacionalidad) %>%     ##nacionalidad
       select(Pais, all_of(columnas_gasto))
     
     datos_gathered <- datos_filtrados %>%
@@ -94,7 +131,111 @@ server <- function(input, output) {
       geom_bar(stat = "identity") +
       theme_minimal() +
       labs(x = "Tipo de Gasto", y = "Gasto por Persona", 
-           title = paste("Gastos por Persona para la nacionalidad:", input$Pais)) +
+           title = paste("Gastos por Persona para la nacionalidad:", input$nacionalidad)) +
+      theme(axis.text.x = element_blank())
+  })
+  
+  output$gastosPlote <- renderPlot({
+    datos_filtrados <- receptivo_fecha %>% 
+      filter(Estudio == input$Educacion) %>%        ##educacion
+      select(Estudio, all_of(columnas_gasto))
+    
+    datos_gathered <- datos_filtrados %>%
+      gather(key = "TipoGasto", value = "Gasto", -Estudio) %>% 
+      mutate(TipoGasto = case_match(
+        TipoGasto,
+        "GastoAlojamiento_porPersona" ~ "Gasto en Alojamiento",
+        "GastoAlimentacion_porPersona" ~ "Gasto en Alimentacion",
+        "GastoTransporte_porPersona" ~ "Gasto en Transporte", 
+        "GastoCultural_porPersona" ~ "Gasto Cultural", 
+        "GastoTours_porPersona" ~ "Gasto en Tours", 
+        "GastoCompras_porPersona" ~ "Gasto en Compras", 
+        "GastoOtros_porPersona" ~ "Otros Gastos"
+      ))
+    
+    ggplot(data = datos_gathered, aes(x = reorder(TipoGasto, Gasto), y = Gasto, fill = TipoGasto)) +
+      geom_bar(stat = "identity") +
+      theme_minimal() +
+      labs(x = "Tipo de Gasto", y = "Gasto por Persona", 
+           title = paste("Gastos por Persona para nivel de estudio:", input$Educacion)) +
+      theme(axis.text.x = element_blank())
+  })
+  
+  output$gastosPloto <- renderPlot({
+    datos_filtrados <- receptivo_fecha %>% 
+      filter(Ocupacion == input$ocupacion) %>%        ##ocupacion
+      select(Ocupacion, all_of(columnas_gasto))
+    
+    datos_gathered <- datos_filtrados %>%
+      gather(key = "TipoGasto", value = "Gasto", -Ocupacion) %>% 
+      mutate(TipoGasto = case_match(
+        TipoGasto,
+        "GastoAlojamiento_porPersona" ~ "Gasto en Alojamiento",
+        "GastoAlimentacion_porPersona" ~ "Gasto en Alimentacion",
+        "GastoTransporte_porPersona" ~ "Gasto en Transporte", 
+        "GastoCultural_porPersona" ~ "Gasto Cultural", 
+        "GastoTours_porPersona" ~ "Gasto en Tours", 
+        "GastoCompras_porPersona" ~ "Gasto en Compras", 
+        "GastoOtros_porPersona" ~ "Otros Gastos"
+      ))
+    
+    ggplot(data = datos_gathered, aes(x = reorder(TipoGasto, Gasto), y = Gasto, fill = TipoGasto)) +
+      geom_bar(stat = "identity") +
+      theme_minimal() +
+      labs(x = "Tipo de Gasto", y = "Gasto por Persona", 
+           title = paste("Gastos por Persona para nivel de estudio:", input$ocupacion)) +
+      theme(axis.text.x = element_blank())
+  })
+  
+  output$gastosPloti <- renderPlot({
+    datos_filtrados <- receptivo_fecha %>% 
+      filter(`Transporte Internacional de Ingreso` == input$ingreso) %>%        ##ingreso
+      select(`Transporte Internacional de Ingreso`, all_of(columnas_gasto))
+    
+    datos_gathered <- datos_filtrados %>%
+      gather(key = "TipoGasto", value = "Gasto", -`Transporte Internacional de Ingreso`) %>% 
+      mutate(TipoGasto = case_match(
+        TipoGasto,
+        "GastoAlojamiento_porPersona" ~ "Gasto en Alojamiento",
+        "GastoAlimentacion_porPersona" ~ "Gasto en Alimentacion",
+        "GastoTransporte_porPersona" ~ "Gasto en Transporte", 
+        "GastoCultural_porPersona" ~ "Gasto Cultural", 
+        "GastoTours_porPersona" ~ "Gasto en Tours", 
+        "GastoCompras_porPersona" ~ "Gasto en Compras", 
+        "GastoOtros_porPersona" ~ "Otros Gastos"
+      ))
+    
+    ggplot(data = datos_gathered, aes(x = reorder(TipoGasto, Gasto), y = Gasto, fill = TipoGasto)) +
+      geom_bar(stat = "identity") +
+      theme_minimal() +
+      labs(x = "Tipo de Gasto", y = "Gasto por Persona", 
+           title = paste("Gastos por Persona para nivel de estudio:", input$ingreso)) +
+      theme(axis.text.x = element_blank())
+  })
+  
+  output$gastosPloteg <- renderPlot({
+    datos_filtrados <- receptivo_fecha %>% 
+      filter(`Transporte Internacional de Egreso` == input$egreso) %>%        ##egreso
+      select(`Transporte Internacional de Egreso`, all_of(columnas_gasto))
+    
+    datos_gathered <- datos_filtrados %>%
+      gather(key = "TipoGasto", value = "Gasto", -`Transporte Internacional de Egreso`) %>% 
+      mutate(TipoGasto = case_match(
+        TipoGasto,
+        "GastoAlojamiento_porPersona" ~ "Gasto en Alojamiento",
+        "GastoAlimentacion_porPersona" ~ "Gasto en Alimentacion",
+        "GastoTransporte_porPersona" ~ "Gasto en Transporte", 
+        "GastoCultural_porPersona" ~ "Gasto Cultural", 
+        "GastoTours_porPersona" ~ "Gasto en Tours", 
+        "GastoCompras_porPersona" ~ "Gasto en Compras", 
+        "GastoOtros_porPersona" ~ "Otros Gastos"
+      ))
+    
+    ggplot(data = datos_gathered, aes(x = reorder(TipoGasto, Gasto), y = Gasto, fill = TipoGasto)) +
+      geom_bar(stat = "identity") +
+      theme_minimal() +
+      labs(x = "Tipo de Gasto", y = "Gasto por Persona", 
+           title = paste("Gastos por Persona para nivel de estudio:", input$egreso)) +
       theme(axis.text.x = element_blank())
   })
 }
