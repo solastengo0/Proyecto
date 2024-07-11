c6cf1aae26492b5b7b46a52a69cd3b333a001f01
Mauro Lopreten <mloprete@cognus.cl>
2024-07-11 14:47:09 -0300

    Modelado: Pruebas y el problema es que el modelo NO ajusta bien. Probe tunnear el forest y aún así hay problemas, comenten estos problemas y den una explicación "del diagnostico" y sigan adelantr

--- borrador modelo.R
+++ borrador modelo.R
@@ -1,4 +1,5 @@
-datos <- read_excel(here("data","receptivo_fecha.xlsx"))
+
+
 
 library(here)
 library(tidyverse)
@@ -8,8 +9,12 @@ library(readxl)
 library(rpart)
 library(mice)
 
+datos <- read_excel(here("data","receptivo_fecha.xlsx"))
+datos$GastoTotal_porPersona = datos$GastoTotal
+
 variablesElegidas <- c("GastoTotal_porPersona", "Motivo", "Estudio", "Alojamiento", "Ocupacion", "TransporteLocal")
 datos2 <- datos[variablesElegidas]
+datos2[is.na(datos2$Estudio),'Estudio'] = 'Sin informacion'
 
 set.seed(33) 
 
@@ -17,31 +22,122 @@ entrenamiento <- sample(1:nrow(datos2), 0.7 * nrow(datos2))
 desarrollo <- datos2[entrenamiento, ]
 testeo <- datos2[-entrenamiento, ]
 
-desarrollo <- na.omit(desarrollo)
-testeo <- na.omit(testeo)
+#desarrollo <- na.omit(desarrollo)
+#testeo <- na.omit(testeo)
 
 #arbol
 
-arbol <- rpart(GastoTotal_porPersona ~ ., data = desarrollo, method = "anova")
+arbol <- rpart(GastoTotal_porPersona ~ ., data = desarrollo)
+
+# Entrenamiento
+
+arbol_predict_training = predict(arbol, desarrollo)
+error_training = sqrt(mean((arbol_predict_training-desarrollo$GastoTotal_porPersona)^2))
+
+# En términos de la escala no parece muygrande
+
+df = data.frame(
+  predicciones = arbol_predict_training,
+  valores_reales = desarrollo$GastoTotal_porPersona
+)
+
+
+# Vemos que el modelo subestima el gasto!!!!
+ggplot(
+  df,
+  aes(
+    x = predicciones,
+    y = valores_reales
+  )
+) + 
+geom_point()
+
+
+
+## El árbol no esta captando las relaciones de variables
+
+
+
+# Revisar test
 
 arbol_predict <- predict(arbol, testeo)
-t_arbol <- table(testeo$GastoTotal_porPersona, arbol_predict)
+error_test = sqrt(mean((arbol_predict-testeo$GastoTotal_porPersona)^2))
 
+df = data.frame(
+  predicciones = arbol_predict,
+  valores_reales = testeo$GastoTotal_porPersona
+)
+
+
+# pasa lo mismo en test
+
+ggplot(
+  df,
+  aes(
+    x = predicciones,
+    y = valores_reales
+  )
+) + 
+  geom_point()
+
+
+# No es un problema de clásificación no se debe hacer un table!!
 
 #bosque
 
-bosque <- randomForest(GastoTotal_porPersona ~ ., data = desarrollo)
+#bosque <- randomForest(GastoTotal_porPersona ~ ., data = desarrollo)
+
+bosque = ranger::ranger(GastoTotal_porPersona ~ ., data = desarrollo)
+
+# Muy pooco ajuste (ver r2)
+
+## Vamos a ver si con un tuneo sirve (aunque es entendible, con variables categorias podria ser muy dificil)
+
+tune_grid <- expand.grid(
+  mtry = c(2, 3, 4, 5),  # Número de variables consideradas en cada división
+  splitrule = "variance", 
+  min.node.size = c(1, 5, 10)  # Tamaño mínimo del nodo
+)
+
+library(caret)
+library(ranger)
+
+# Configuración de control de entrenamiento (lo dejo como ultima instancia)
+control <- trainControl(
+  method = "cv",
+  number = 5,
+  search = "grid"
+)
+
+
+
+modelo_tuneado <- train(
+  GastoTotal_porPersona ~ .,
+  data = desarrollo,
+  method = "ranger",
+  trControl = control,
+  tuneGrid = tune_grid,
+  metric = "RMSE"
+)
+
+
+# El ajuste sigue dando mal... esto puede ser una conclusión también expliquen los pasos que hicieron
+# e interpreten xq el modelo esta dando mal (que subestima los valores del gasto) posibles mejoras que si 
+# tuvieran X datos podrian mejorar, etc.
 
 bosque_predict <- predict(bosque, testeo)
-t_bosque <- table(testeo$GastoTotal_porPersona, bosque_predict)
+
+error_test = sum(bosque_predict)
+
+#t_bosque <- table(testeo$GastoTotal_porPersona, bosque_predict) (No es clasificación)
 
 importancia_variables <- importance(bosque)
 importancia_variables 
 
 #evaluacion del modelo
-#EMC (error medio cuadratico)
 
-mean_gasto <- mean(datos2$GastoTotal_porPersona, na.rm = TRUE)
+
+
 
 
 emc_arbol <- mean((testeo$GastoTotal_porPersona - arbol_predict)^2)
